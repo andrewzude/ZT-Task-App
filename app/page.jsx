de7a88ecb8b85136324613a25d9b8d976d790523
ZT <zawthet.naing@gmail.com>
2026-03-18 07:31:20 +0000

    first commit

---
+++ app/page.jsx
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <div className="p-10 text-center">
+      <h1 className="text-4xl font-bold">ZT Task Manager</h1>
+      <p>App is successfully deployed!</p>
+    </div>
+  );
+}
